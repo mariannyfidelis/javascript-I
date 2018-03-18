7f1ead86d787a5ff9254fd9c5e0e9fba9d486eba
mariannyfidelis <mariannyfidelis@gmail.com>
2018-03-18 00:36:42 -0300

    Primeiro commit

---
+++ introducao-javascript/js/buscar-paciente.js
@@ -0,0 +1,29 @@
+var botao = document.querySelector("#buscar-paciente");
+
+botao.addEventListener("click", function(){
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function(){
+        
+        if(xhr.status == 200){
+            
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente){
+                 adicionaPacienteNaTabela(paciente);
+            }); 
+        }
+        else{
+           console.log("Erro na requisição ...."); 
+        }
+    });  
+    
+    xhr.send();
+});
+    
+
+
+
